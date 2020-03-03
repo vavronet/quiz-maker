9cd02ba3956d5b1350d6fedcba39e069e26bde32
Christiana Vucea <christiana@vav.ro>
2020-03-02 20:44:46 -0500

    Added User Registration

--- main.py
+++ main.py
@@ -1,1 +1,3 @@
-print('Welcome to Quiz Maker, a program where you can make and take quizes and tests.')
+print('Welcome to Quiz Maker, a program where you can make and take quizes and tests. \n')
+name = input('Please enter your name: \n')
+print('Hello, '+name+'!')
