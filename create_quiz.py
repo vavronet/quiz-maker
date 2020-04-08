3451eaee717e8429aab36be865b947fd59e6bfa9
Christiana Vucea <christiana@vav.ro>
2020-04-07 20:23:28 -0400

    QM-005 - Added subjects

--- create_quiz.py
+++ create_quiz.py
@@ -1,5 +1,15 @@
+import subjects
+
 def run():
-    print('You have chosen to create a quiz.')
+    print('You have chosen to create a quiz.\n') 
+
+    input_text = 'Please choose the subject you want your quiz to fall under:\n'
+    
+    for x, y in subjects.subjects.items():
+        input_text = input_text + ' - Type {} for {}\n'.format(x,y)
+
+    subject = input(input_text)
+
 
     #Ask for subject
 
---
+++ subjects.py
@@ -0,0 +1,8 @@
+subjects = {
+    '1': 'Math',
+    '2': 'History',
+    '3': 'Science',
+    '4': 'Music',
+    '5': 'Cartoon References',
+}
+ 
